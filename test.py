17d04000a561d3b04078629f10ada9114be8ef0d
Choi-Jae-seok <67653629+Choi-Jae-seok@users.noreply.github.com>
2025-04-16 16:57:47 +0900

    test.py

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+st.markdown("""
+    <style>
+        .css-1d391kg {  /* 사이드바 넓이 */
+            width: 200px !important;
+        }
+        .block-container {
+            max-width: 1000px !important;
+            margin: auto;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
 #태블로 추가
 tableau_embed1 = """
 <div class='tableauPlaceholder' id='viz1744784959429' style='position: relative'><noscript><a href='#'><img alt='대시보드 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;gv&#47;gvc_17447849130600&#47;1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='gvc_17447849130600&#47;1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;gv&#47;gvc_17447849130600&#47;1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1744784959429');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1200px';vizElement.style.height='1827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1200px';vizElement.style.height='1827px';} else { vizElement.style.width='100%';vizElement.style.height='1677px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
@@ -90,7 +102,7 @@ elif add_selectbox == "글로벌 공급망":
     tab1, tab2 = st.tabs(["수출입 기업 품목별 거래 정보 조회", "운송 공급망 조회"])
     with tab1:
         st.text("(준비중입니다)")
-        st.components.v1.html(tableau_embed1, height=1900, width=1300)
+        st.components.v1.html(tableau_embed1, height=1820)
     with tab2:
         st.text("(준비중입니다)")
 
